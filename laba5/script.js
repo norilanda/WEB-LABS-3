c6daf9540bff41bd669c13ce3cc5f2bd8d726269
Lidia Makarchuk <lidiamakarcuk9@gmail.com>
2022-11-16 00:00:20 +0200

    Add part of 3rd task

--- laba5/script.js
+++ laba5/script.js
@@ -1,3 +1,4 @@
+//1
 function exchangeTitles()
 {
     var textUpperH = document.getElementById("upperHeading").innerText;
@@ -7,3 +8,76 @@ function exchangeTitles()
 }
 document.getElementById("upperHeading").onclick = function() {exchangeTitles()};
 document.getElementById("downHeading").onclick = function() {exchangeTitles()};
+
+//------------------------------------------------------------------------
+//2
+function calcParallelogramS(a, h)
+{    
+    return a*h;
+}
+function checkNumbers(a, h)
+{
+    var isEverythingOk = true;
+    if(!a || !h)
+    {
+        alert("Enter all numbers!");
+        isEverythingOk = false;
+    }
+    else if (a <= 0 && h <=0)
+    {
+        alert("numbers should be positive!");
+        isEverythingOk = false;
+    }
+    else if(a <= 0){
+        alert("'a' should be positive!");
+        isEverythingOk = false;
+    }    
+    else if (h <=0)
+    {
+        alert("'h' should be positive!");
+        isEverythingOk = false;
+    }
+    return isEverythingOk;
+}
+function displayS(elementID)
+{
+    var a = document.getElementById("task2_a").value ;
+    var h = document.getElementById("task2_h").value ;
+    console.log("a"+ a);
+    if(checkNumbers(a, h))
+    {
+        S = calcParallelogramS(a, h);
+        document.getElementById(elementID).insertAdjacentHTML('beforeend', "Parallelogram with a = " + a + " and h = "+ h + " has Square = "+S + " <br /> ");
+    }    
+}
+elementID = "div3";
+document.getElementById("task2_S").onclick = function() {displayS(elementID)};
+
+//------------------------------------------------------------------------
+//3
+function calcWords(text)
+{
+    var splitedText = text.split(" "); 
+    var wordsNumber = 0;
+    for (let i = 0; i < splitedText.length; i++)
+    {
+        for (let j = 0; j < splitedText[i].length; j++)
+        {     
+            var code = splitedText[i].charCodeAt(j);
+            if ((code > 47 && code < 58) || // numeric (0-9)
+                (code > 64 && code < 91) || // upper alpha (A-Z)
+                (code > 96 && code < 123))// lower alpha (a-z)
+            {
+                wordsNumber++;
+                break;
+            }
+        }
+    }
+    return wordsNumber;
+}
+function displayCountedWords()
+{
+    var text = document.getElementById("task3_text").value ;
+    var wordsNumber = calcWords(text);
+    alert("Number of words in text = " + wordsNumber);
+}
