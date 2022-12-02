3753a4f112d2e2a13d678a1b5ebedd40cdada7d7
Lidia Makarchuk <lidiamakarcuk9@gmail.com>
2022-12-02 20:37:38 +0200

    just testing

--- laba6/script.js
+++ laba6/script.js
@@ -10,9 +10,38 @@ function CreateNotificationBar()
     var notificationBar = '<div class="notificaton"> <p> '+content+ '</p> <p id="ok"> ✔ </p>' + '</div>';
     
     document.body.insertAdjacentHTML('afterbegin', notificationBar);
-    // setTimeout(function(){return document.querySelector(".notificaton").animate({height:"toggle"},"slow")},450);
+
+    let formData = new FormData();
+    formData.append("content", content);
+       f(formData);
    
+
+    // var elem = document.querySelector(".notificaton");
+    // var tick = 0;
+    // var timer = setInterval(function() {
+    //     if (tick < 100) {
+    //     //elem.style.position = "absolute";
+    //     elem.style.height = tick + 'px';
+    //     tick++;
+    //     } else {
+    //     clearInterval(timer)
+    //     }
+    // }, 10)
+
 }
+
+async function f(formData)
+{
+     let response = await fetch('https://norilanda.github.io/WEB-LABS-3', {
+        method: 'POST',
+        body: formData
+      });
+    //   let result = await response.json();
+    // //   alert(result.message);
+    // console.log(result);  
+    // https://jsonplaceholder.typicode.com/posts
+}
+
 
 document.getElementById("submit_creation").onclick = function() {CreateNotificationBar();}
 
