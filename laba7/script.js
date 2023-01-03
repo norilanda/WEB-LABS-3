767f4dd541787ee26dfec8c95f72e07ea81f36e0
Lidia Makarchuk <lidiamakarcuk9@gmail.com>
2023-01-03 12:26:38 +0200

    Add animation for blue square, starting from one corner

--- laba7/script.js
+++ laba7/script.js
@@ -5,6 +5,7 @@ const ctx = animRegion.getContext('2d');
 // buttons
 const button_play = document.getElementById('play');
 const button_close = document.getElementById('close');
+const button_start = document.getElementById('start');
 
 button_play.onclick = function() {
     workRegion.style.display = 'block';
@@ -46,4 +47,110 @@ function fillSquareWithImagePattern(startX, startY, sideSize, imagePath)
         ctx.rect(startX, startY, sideSize, sideSize);
         ctx.fill();
     };
+}
+
+//start animation
+button_start.onclick = function() {
+    let buttonName = button_start.innerHTML;
+    if(buttonName == "start")
+    {
+        button_start.disabled = true;
+        placeSquare(3);
+        button_start.innerHTML = "reload";
+        button_start.disabled = false;
+    }
+    else
+    {
+        button_start.innerHTML = "start";
+    }
+}
+
+function placeSquare(numberOfOuterSquare)
+{
+    const sideSize = 20;
+    //--make a function
+    let squareSide = Math.min(animRegion.height, animRegion.width);
+    let littleSquareSide = squareSide/2;
+    let startPointX = (animRegion.width - squareSide)/2;
+    let startPointY = (animRegion.height - squareSide)/2;
+    //--make a function
+    let startX ;
+    let startY ;
+    let sideSizeWidth = sideSize;
+    let sideSizeHeight = sideSize;
+    switch(numberOfOuterSquare)
+    {
+        case 1:{
+            startX = startPointX + littleSquareSide - sideSize;
+            startY = startPointY + littleSquareSide - sideSize;
+            break;
+        }
+        case 2:
+            {
+                startX = startPointX + littleSquareSide;
+                startY = startPointY + littleSquareSide - sideSize;
+                break;
+            }
+        case 3:
+            {
+                startX = startPointX + littleSquareSide - sideSize;
+                startY = startPointY + littleSquareSide;
+                break;
+            }
+        case 4:
+            {
+                startX = startPointX + littleSquareSide;
+                startY = startPointY + littleSquareSide;
+                break;
+            }
+    }
+
+    ctx.beginPath();
+    ctx.fillStyle = 'blue';
+    ctx.rect(startX, startY, sideSize, sideSize);
+    ctx.fill();
+
+    let dirrection = 0;
+    let i = 0;
+  
+    var interval = setInterval(function() {
+        animateSquare(startX, startY, sideSizeWidth, sideSizeHeight);
+        switch (dirrection%4)
+        {
+            case 0:{//right
+                sideSizeWidth++;
+                break;
+            }
+            case 1:{//up
+                startY--;
+                sideSizeHeight++;
+                break;
+            }
+            case 2:{//left
+                startX--;
+                sideSizeWidth++;
+                break;
+            }
+            case 3:{//down
+                sideSizeHeight++;
+                break;
+            }
+        }
+        dirrection++;
+        i++; 
+        if (hasExceed(startX, startY, sideSizeWidth, sideSizeHeight)) { clearInterval(interval); }
+            }, 30);
+}
+function hasExceed(startX, startY, sideSizeWidth, sideSizeHeight)
+{
+    if (startX==0||startY==0 || startX + sideSizeWidth > animRegion.width || startY + sideSizeHeight > animRegion.height)
+    {return true;}
+    return false;
+}
+function animateSquare(startX, startY, sideSizeWidth, sideSizeHeight)
+{
+    ctx.beginPath();
+    ctx.fillStyle = 'blue';
+    ctx.rect(startX, startY, sideSizeWidth, sideSizeHeight);
+    ctx.fill();
 }
